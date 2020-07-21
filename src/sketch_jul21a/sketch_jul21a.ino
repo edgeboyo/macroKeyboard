1e6db6ec0ab893fbf85a79301c5b278008e1e516
edgeboyo <mkolybko@gmail.com>
2020-07-21 22:20:17 +0200

    Macro key for F13 programmed

---
+++ src/sketch_jul21a/sketch_jul21a.ino
@@ -0,0 +1,15 @@
+#include <Keyboard.h>
+
+
+void setup() {
+  // put your setup code here, to run once:
+  delay(2000);
+  Keyboard.begin();
+  Keyboard.press(240);
+  Keyboard.end();
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:I love you
+
+}
