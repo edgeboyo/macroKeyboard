40a85b210d989bd1eb58499c366189130cf83b25
edgeboyo <mkolybko@gmail.com>
2020-08-18 16:24:08 +0200

    Got the board scheme v1 and the bitmaps of waifus

--- src/sketch_jul21a/sketch_jul21a.ino
+++ src/sketch_jul21a/sketch_jul21a.ino
@@ -3,13 +3,23 @@
 
 void setup() {
   // put your setup code here, to run once:
-  delay(2000);
   Keyboard.begin();
-  Keyboard.press(240);
-  Keyboard.end();
+  pinMode(8, OUTPUT);
+  pinMode(12, INPUT);
+  
 }
 
 void loop() {
   // put your main code here, to run repeatedly:I love you
-
+  digitalWrite(8, HIGH);
+  if(digitalRead(12) == HIGH){
+    Serial.write("H");
+    //Keyboard.print("H");
+    delay(200);
+  }
+  else {
+    Serial.write("L");
+    delay(200);
+  }
+  digitalWrite(8, LOW);
 }
